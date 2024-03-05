fb44c11b13f3ce142cba80701499282697192ee3
ttoy12 <thomastoy1415@gmail.com>
2024-03-04 23:30:48 -0800

    Update testing-view.jsx

--- src/sections/testing/view/testing-view.jsx
+++ src/sections/testing/view/testing-view.jsx
@@ -1,37 +1,61 @@
 import * as React from 'react';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+import Iconify from 'src/components/iconify';
+
+function createData(name, id, role, items, status) {
+  return { name, id, role, items, status };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Bob Joe', 'J1D2xYz', 'Front Desk', 10, 'Good'),
+  createData('Bob Joe', 'J1D2xYz', 'Front Desk', 10, 'Good'),
+  createData('Jobby Steve', 'Ja2zxYz', 'Chief', 30, 'Good'),
+  createData('Mary Jane', 'a2xBz', 'Officer', 2, 'Good'),
+  createData('Bary Gary', 'B2qAd', 'Officer', 3, 'Bad'),
+  createData('Bob Joe', 'J1D2xYz', 'Front Desk', 10, 'Good'),
+  createData('Bob Joe', 'J1D2xYz', 'Front Desk', 10, 'Good'),
+  createData('Jobby Steve', 'Ja2zxYz', 'Chief', 30, 'Good'),
+  createData('Mary Jane', 'a2xBz', 'Officer', 2, 'Good'),
+  createData('Bary Gary', 'B2qAd', 'Officer', 3, 'Bad'),
 ];
 
 export default function TestingView() {
   return (
     <Container>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px", minWidth: 650 }}>
+        <Typography variant="h4" sx={{ flexGrow: 1 }}>Inventory</Typography>
+        <Stack direction="row" spacing={1}>
+          <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+            New Item
+          </Button>
+          <Button variant="contained" color="inherit">
+            <Iconify icon="ic:round-filter-list" />
+            Filter
+          </Button>
+          <Button variant="contained" color="inherit">Download</Button>
+        </Stack>
+      </Box>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">ID</TableCell>
+              <TableCell align="right">Role</TableCell>
+              <TableCell align="right">Items</TableCell>
+              <TableCell align="right">Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -43,10 +67,10 @@ export default function TestingView() {
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.id}</TableCell>
+                <TableCell align="right">{row.role}</TableCell>
+                <TableCell align="right">{row.items}</TableCell>
+                <TableCell align="right">{row.status}</TableCell>
               </TableRow>
             ))}
           </TableBody>
